6f7ce316d286d2e589d7f2ad61a67b4a5cd33cfd
Scott James Remnant <scott@netsplit.com>
2018-06-15 20:48:53 -0700

    Add frameworks for the database

---
+++ Database iOS/Database.h
@@ -0,0 +1,19 @@
+//
+//  Database.h
+//  Database
+//
+//  Created by Scott James Remnant on 6/15/18.
+//  Copyright © 2018 Scott James Remnant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Database.
+FOUNDATION_EXPORT double DatabaseVersionNumber;
+
+//! Project version string for Database.
+FOUNDATION_EXPORT const unsigned char DatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Database/PublicHeader.h>
+
+
