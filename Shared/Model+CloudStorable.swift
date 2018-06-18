5483ee5a077778b4145f5b9e14aa35d2bbb2c47b
Scott James Remnant <scott@netsplit.com>
2018-06-18 16:25:11 -0700

    Model - update model slightly
    By making detailPartsFitted a strict subset of detailParts, we can simplify all the crazy set code. First we just treat detailParts the same as the others to generate the tables, and then we do a pass over it to adjust the isFitted values based on whether or not the title is in the other field.
    
    At the same time, match the model rename of lights/speakerFittings

--- Shared/Model+CloudStorable.swift
+++ Shared/Model+CloudStorable.swift
@@ -19,37 +19,6 @@ extension Model : CloudStorable {
     ///
     /// - Parameters:
     ///   - objects: set of objects in the relationship.
-    ///   - values: set of titles from CloudKit.
-    ///
-    /// - Returns: (`newValues`, `removeObjects`) where `newValues` contains the set of titles
-    ///   from `values` that were not present in the relationship, and `removeObjects` contains the
-    ///   set of objects from `objects` that were not present in the title list.
-    private func differencesFrom(objects: NSSet?, to values: Set<String>) -> (Set<String>, NSSet) {
-        if let objects = objects as? Set<NSManagedObject> {
-            let removeObjects = objects.filter {
-                guard let title = $0.value(forKey: "title") as? String else { return true }
-                return !values.contains(title)
-                } as NSSet
-
-            let newValues = values.subtracting(objects.map {
-                guard let title = $0.value(forKey: "title") as? String else { return "" }
-                return title
-            })
-
-            return (newValues, removeObjects)
-        } else {
-            return (values, [])
-        }
-    }
-
-    /// Identify the differences from relationship objects to a `CKRecord` list.
-    ///
-    /// Since Core Data has no concept of a string list, and to reduce the model burden on the
-    /// Cloud Kit side, we have to translate between string lists and relationships with sets of
-    /// objects that just contain a title.
-    ///
-    /// - Parameters:
-    ///   - objects: set of objects in the relationship.
     ///   - record: CloudKit record.
     ///   - key: key of field in CloudKit.
     ///   - as: type of `NSManagedObject` to construct.
@@ -60,7 +29,19 @@ extension Model : CloudStorable {
     ///   `record`.
     private func differencesFrom<T : NSManagedObject>(objects: NSSet?, to record: CKRecord, key: String, as type: T.Type) -> (NSSet, NSSet) {
         if let values = record[key] as? [String] {
-            let (newValues, removeObjects) = differencesFrom(objects: objects, to: Set(values))
+            var removeObjects = NSSet()
+            var newValues = Set(values)
+            if let objects = objects as? Set<NSManagedObject> {
+                removeObjects = objects.filter {
+                    guard let title = $0.value(forKey: "title") as? String else { return true }
+                    return !newValues.contains(title)
+                } as NSSet
+
+                newValues.subtract(objects.map {
+                    guard let title = $0.value(forKey: "title") as? String else { return "" }
+                    return title
+                })
+            }
 
             let insertObjects = Set(newValues.map { (title: String) -> T in
                 let object = T(context: managedObjectContext!)
@@ -100,12 +81,15 @@ extension Model : CloudStorable {
         addToCouplings(insertCouplings)
         removeFromCouplings(removeCouplings)
 
+        let (insertDetailParts, removeDetailParts) = differencesFrom(objects: detailParts, to: record, key: "detailParts", as: DetailPart.self)
+        addToDetailParts(insertDetailParts)
+        removeFromDetailParts(removeDetailParts)
+
         let (insertFeatures, removeFeatures) = differencesFrom(objects: features, to: record, key: "features", as: Feature.self)
         addToFeatures(insertFeatures)
         removeFromFeatures(removeFeatures)
 
-        // FIXME: rename the schema "lighting" to "lights
-        let (insertLights, removeLights) = differencesFrom(objects: lights, to: record, key: "lighting", as: Light.self)
+        let (insertLights, removeLights) = differencesFrom(objects: lights, to: record, key: "lights", as: Light.self)
         addToLights(insertLights)
         removeFromLights(removeLights)
 
@@ -121,33 +105,17 @@ extension Model : CloudStorable {
         addToTasks(insertTasks)
         removeFromTasks(removeTasks)
 
-        // Handle the split detailParts/detailPartsFitted field.
-        // FIXME: evalulate whether maintaining this split is the best way to handle this, or if we should just have `DetailPart` and `DetailPartFitted` on this side.
-        let newDetailParts: Set<String>
-        if let values = record["detailParts"] as? [String] {
-            newDetailParts = Set(values)
-        } else {
-            newDetailParts = []
+        // The detailPartsFitted field is a subset of detailParts, which is updated above, so we
+        // update it by checking the set of objects that need to have isFitted changed.
+        if let detailParts = detailParts as? Set<DetailPart> {
+            if let detailPartsFitted = record["detailPartsFitted"] as? [String] {
+                for detailPart in detailParts {
+                    detailPart.isFitted = detailPartsFitted.contains(detailPart.title ?? "")
+                }
+            } else {
+                for detailPart in detailParts { detailPart.isFitted = false }
+            }
         }
-
-        let newDetailPartsFitted: Set<String>
-        if let values = record["detailPartsFitted"] as? [String] {
-            newDetailPartsFitted = Set(values)
-        } else {
-            newDetailPartsFitted = []
-        }
-
-        let (newValues, removeDetailParts) = differencesFrom(objects: detailParts, to: newDetailParts.union(newDetailPartsFitted))
-
-        let insertDetailParts = Set(newValues.map { (title: String) -> DetailPart in
-            let detailPart = DetailPart(context: managedObjectContext!)
-            detailPart.title = title
-            detailPart.isFitted = newDetailPartsFitted.contains(title)
-            return detailPart
-        }) as NSSet
-
-        addToDetailParts(insertDetailParts)
-        removeFromDetailParts(removeDetailParts)
 
         if let reference = record["purchase"] as? CKRecord.Reference {
             purchase = try Purchase.forRecordID(reference.recordID, in: managedObjectContext!)
