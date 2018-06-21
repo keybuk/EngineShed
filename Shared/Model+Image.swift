9c45a3f519b3f025168bba2c37b6cc21a5d91d5b
Scott James Remnant <scott@netsplit.com>
2018-06-21 12:41:18 -0700

    Model+Image - support deletion in batch requests too
    Deleting the image accompanying the model can be tricky, it's fine when we have an NSManagedObject around because prepareDeletion() will get called - cleanup that case a little to explicitly delete rather than going through a property modifier, and ignore the "no such file" error entirely because such objects can exist in multiple contexts.
    
    But it's harder when we issue a batch delete, we need to be able to run the same query but just check for image filenames, iterate over them, and delete them out - if possible avoiding actually doing a full fetch. So to accomplish this we add a class function to NSManagedObject and override that in Model; now we can do NSMO.willDeleteObjects(request, in: context) and know that for a model, that will result in the images being deleted.

--- Shared/Model+Image.swift
+++ Shared/Model+Image.swift
@@ -7,16 +7,15 @@
 //
 
 import Foundation
+import CoreData
 
 extension Model {
 
-    private var imagesURL: URL {
-        return FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appendingPathComponent("ModelImages", isDirectory: true)
-    }
+    private static let imagesURL: URL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appendingPathComponent("ModelImages", isDirectory: true)
 
     internal var imageURL: URL? {
         guard let imageFilename = imageFilename else { return nil }
-        return imagesURL.appendingPathComponent(imageFilename)
+        return Model.imagesURL.appendingPathComponent(imageFilename)
     }
 
     public var image: ModelImage? {
@@ -33,7 +32,7 @@ extension Model {
                     // Ignore, no matter how strange that is.
                     print("Failed to delete file that wasn't there: \(imageURL.lastPathComponent)")
                 } catch {
-                    fatalError("Unhandled error \(error)")
+                    print("Failed to delete \(imageURL.lastPathComponent): \(error)")
                 }
             }
 
@@ -41,7 +40,7 @@ extension Model {
 
             if let image = newValue {
                 imageFilename = UUID().uuidString + ".png"
-                try! FileManager.default.createDirectory(at: imagesURL, withIntermediateDirectories: true, attributes: nil)
+                try! FileManager.default.createDirectory(at: Model.imagesURL, withIntermediateDirectories: true, attributes: nil)
 
                 try! image.pngData()?.write(to: imageURL!)
             }
@@ -51,8 +50,41 @@ extension Model {
     public override func prepareForDeletion() {
         super.prepareForDeletion()
 
-        // Trigger removal of the existing image file.
-        image = nil
+        if let imageURL = imageURL {
+            do {
+                try FileManager.default.removeItem(at: imageURL)
+            } catch CocoaError.fileNoSuchFile {
+                // Ignore, this is probably one context learning about the deletion of another.
+            } catch {
+                print("Failed to delete \(imageURL.lastPathComponent): \(error)")
+            }
+        }
+    }
+
+    @objc
+    override class func willDeleteObjects(matching deleteRequest: NSFetchRequest<NSFetchRequestResult>, in context: NSManagedObjectContext) throws {
+        // Create a new fetch request, with the same predicate; both remembering that deleteRequest
+        // is a reference, and also that we only really care about the predicate.
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = self.fetchRequest()
+
+        fetchRequest.predicate = deleteRequest.predicate
+        fetchRequest.fetchBatchSize = 20
+        fetchRequest.resultType = .dictionaryResultType
+        fetchRequest.propertiesToFetch = [ "imageFilename" ]
+
+        let results = try context.fetch(fetchRequest) as! [[String: Any]]
+        for result in results {
+            guard let imageFilename = result["imageFilename"] as? String else { continue }
+            let imageURL = Model.imagesURL.appendingPathComponent(imageFilename)
+
+            do {
+                try FileManager.default.removeItem(at: imageURL)
+            } catch CocoaError.fileNoSuchFile {
+                // Ignore.
+            } catch {
+                print("Failed to delete \(imageURL.lastPathComponent): \(error)")
+            }
+        }
     }
 
 }
---
+++ Shared/NSManagedObject+WillDelete.swift
@@ -0,0 +1,18 @@
+//
+//  NSManagedObject+WillDelete.swift
+//  EngineShed
+//
+//  Created by Scott James Remnant on 6/21/18.
+//  Copyright © 2018 Scott James Remnant. All rights reserved.
+//
+
+import CoreData
+
+
+// Extend NSManagedObject to support custom delete handling during batch operations.
+extension NSManagedObject {
+
+    @objc
+    class func willDeleteObjects(matching deleteRequest: NSFetchRequest<NSFetchRequestResult>, in context: NSManagedObjectContext) throws {}
+}
+
