8dcd1f6a8a68c8a8c815ef03c36c75a1c6178fe1
Scott James Remnant <scott@netsplit.com>
2018-07-30 22:45:16 -0700

    Model - add static fetchRequestsFor... methods

---
+++ Database/Model+FetchRequests.swift
@@ -0,0 +1,27 @@
+//
+//  Model+FetchRequests.swift
+//  EngineShed
+//
+//  Created by Scott James Remnant on 7/30/18.
+//  Copyright © 2018 Scott James Remnant. All rights reserved.
+//
+
+import CoreData
+
+extension TrainMember {
+
+    public static func fetchRequestForModels(context: NSManagedObjectContext) -> NSFetchRequest<Model> {
+        let fetchRequest: NSFetchRequest<Model> = Model.fetchRequest()
+        fetchRequest.fetchBatchSize = 20
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "modelClass", ascending: true),
+            NSSortDescriptor(key: "number", ascending: true),
+            NSSortDescriptor(key: "name", ascending: true),
+            NSSortDescriptor(key: "dispositionRawValue", ascending: true)
+        ]
+
+        return fetchRequest
+    }
+    
+}
+
---
+++ Database/TrainMember+FetchRequests.swift
@@ -0,0 +1,24 @@
+//
+//  TrainMember+FetchRequests.swift
+//  EngineShed
+//
+//  Created by Scott James Remnant on 7/23/18.
+//  Copyright © 2018 Scott James Remnant. All rights reserved.
+//
+
+import CoreData
+
+extension TrainMember {
+
+    public static func fetchRequestForTrains(context: NSManagedObjectContext) -> NSFetchRequest<TrainMember> {
+        let fetchRequest: NSFetchRequest<TrainMember> = TrainMember.fetchRequest()
+        fetchRequest.fetchBatchSize = 20
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "train.name", ascending: true),
+            NSSortDescriptor(key: "index", ascending: true),
+        ]
+
+        return fetchRequest
+    }
+
+}
