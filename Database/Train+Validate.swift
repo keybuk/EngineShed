f686f3b5d6b8b44a992f892854c76a1c17f9df25
Scott James Remnant <scott@netsplit.com>
2019-01-05 14:26:49 -0800

    Train/TrainMember - validate objects
    Train objects are only valid when their name is set, they have at least one member, and all members are valid.
    
    Train Member objects are only valid when their title is set

---
+++ Database/Train+Validate.swift
@@ -0,0 +1,47 @@
+//
+//  Train+Validate.swift
+//  EngineShed
+//
+//  Created by Scott James Remnant on 1/4/19.
+//  Copyright © 2019 Scott James Remnant. All rights reserved.
+//
+
+import CoreData
+
+extension Train {
+
+    enum ValidationError : Error {
+
+        /// Train name may not be empty.
+        case emptyName
+
+        /// Train must have at least one member.
+        case noMembers
+
+    }
+
+    @objc
+    func validateName(_ value: AutoreleasingUnsafeMutablePointer<AnyObject?>) throws {
+        guard let name = value.pointee as? String, name != "" else { throw ValidationError.emptyName }
+    }
+
+    func validateMembers() throws {
+        guard members!.count > 0 else { throw ValidationError.noMembers }
+
+        for case let member as TrainMember in members! {
+            if member.isInserted { try member.validateForInsert() }
+            else if member.isUpdated { try member.validateForUpdate() }
+        }
+    }
+
+    public override func validateForInsert() throws {
+        try super.validateForInsert()
+        try validateMembers()
+    }
+
+    public override func validateForUpdate() throws {
+        try super.validateForUpdate()
+        try validateMembers()
+    }
+
+}
