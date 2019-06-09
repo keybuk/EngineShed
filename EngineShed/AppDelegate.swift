841f6ab6c2d73c8c684b54ad9f23c8854f9448e4
Scott James Remnant <scott@netsplit.com>
2019-06-09 14:08:34 -0700

    Use the new NSPersistentCloudKitContainer

--- EngineShed/AppDelegate.swift
+++ EngineShed/AppDelegate.swift
@@ -82,14 +82,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     // MARK: - Core Data stack
 
-    lazy var persistentContainer: NSPersistentContainer = {
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "EngineShed")
+        let container = NSPersistentCloudKitContainer(name: "EngineShed")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
