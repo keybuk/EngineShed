4608fc5af882eb10afeac7fdc37da67fdab97f81
Scott James Remnant <scott@netsplit.com>
2019-06-17 18:32:32 -0500

    Add tests for Collection.splitBetween

--- EngineShedTests/SplitBetweenTests.swift
+++ EngineShedTests/SplitBetweenTests.swift
@@ -12,6 +12,34 @@ import XCTest
 
 class SplitBetweenTests : XCTestCase {
 
+    // MARK: splitBetween
+
+    func testSplitBetween() {
+        let integers = [ 1, 2, 3, 2, 3, 4, 4, 3, 4, 6, 1, 1 ]
+        let result = integers.split(between: { $0 > $1 })
+        XCTAssertEqual(result, [[1, 2, 3], [2, 3, 4, 4], [3, 4, 6], [1, 1]])
+    }
+
+    func testSplitBetweenEverything() {
+        let integers = [ 1, 2, 3, 2, 3, 4, 4, 3, 4, 6, 1, 1 ]
+        let result = integers.split(between: { _, _ in true })
+        XCTAssertEqual(result, [[1], [2], [3], [2], [3], [4], [4], [3], [4], [6], [1], [1]])
+    }
+
+    func testSplitBetweenSplitAtLast() {
+        let integers = [ 1, 2, 3, 2, 3, 4, 4, 3, 4, 6, 1, 1 ]
+        let result = integers.split(between: { $0 == $1 })
+        XCTAssertEqual(result, [[ 1, 2, 3, 2, 3, 4], [4, 3, 4, 6, 1], [1]])
+    }
+
+    func testSplitBetweenNotFound() {
+        let integers = [ 1, 2, 3, 2, 3, 4, 4, 3, 4, 6, 1, 1 ]
+        let result = integers.split(between: { $1 - $0 > 2 })
+        XCTAssertEqual(result, [[ 1, 2, 3, 2, 3, 4, 4, 3, 4, 6, 1, 1 ]])
+    }
+
+    // MARK: indexOfAdjacent
+
     func testIndexOfAdjacent() {
         let integers = [ 1, 2, 3, 2, 3, 4, 4, 3, 4, 6, 1, 1 ]
         XCTAssertEqual(integers.indexOfAdjacent(where: { abs($0 - $1) > 1 }), 8)
