ec469c080edb0bbd0a8026df2d3e1273f47df642
Scott James Remnant <scott@netsplit.com>
2020-01-24 23:09:46 -0800

    ConvertibleFromString: Style guide

--- EngineShed/Util/ConvertibleFromString.swift
+++ EngineShed/Util/ConvertibleFromString.swift
@@ -2,20 +2,18 @@
 //  ConvertibleFromString.swift
 //  EngineShed
 //
-//  Created by Scott James Remnant on 7/12/18.
-//  Copyright © 2018 Scott James Remnant. All rights reserved.
+//  Created by Scott James Remnant on 12/19/17.
+//  Copyright © 2017 Scott James Remnant. All rights reserved.
 //
 
 import Foundation
 
 protocol ConvertibleFromString {
-
     init?(describedBy: String)
-
 }
 
-extension ConvertibleFromString where Self : CaseIterable & CustomStringConvertible {
-
+extension ConvertibleFromString
+where Self: CaseIterable & CustomStringConvertible {
     init?(describedBy string: String) {
         if let enumCase = Self.allCases.first(where: { $0.description == string }) {
             self = enumCase
@@ -23,6 +21,4 @@ extension ConvertibleFromString where Self : CaseIterable & CustomStringConverti
             return nil
         }
     }
-
 }
-
