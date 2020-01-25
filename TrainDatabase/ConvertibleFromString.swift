fd01c9e3294668dcf3c782cbfa306f27c7ba8ce2
Scott James Remnant <scott@netsplit.com>
2020-01-24 23:10:02 -0800

    ConvertibleFromString - update from EngineShed

--- TrainDatabase/ConvertibleFromString.swift
+++ TrainDatabase/ConvertibleFromString.swift
@@ -1,6 +1,6 @@
 //
 //  ConvertibleFromString.swift
-//  TrainDatabase
+//  EngineShed
 //
 //  Created by Scott James Remnant on 12/19/17.
 //  Copyright © 2017 Scott James Remnant. All rights reserved.
@@ -9,23 +9,16 @@
 import Foundation
 
 protocol ConvertibleFromString {
-    
     init?(describedBy: String)
-    
 }
 
-extension ConvertibleFromString where Self : CaseIterable & CustomStringConvertible {
-    
+extension ConvertibleFromString
+where Self: CaseIterable & CustomStringConvertible {
     init?(describedBy string: String) {
-        for enumCase in Self.allCases {
-            if string == enumCase.description {
-                self = enumCase
-                return
-            }
+        if let enumCase = Self.allCases.first(where: { $0.description == string }) {
+            self = enumCase
+        } else {
+            return nil
         }
-        
-        return nil
     }
-    
 }
-
