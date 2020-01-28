a60d246e7d0d44f21778b6ca571a48c151edf7a4
Scott James Remnant <scott@netsplit.com>
2020-01-28 10:28:27 -0800

    Import Database from EngineShed and test automatic linking

--- EngineShed/AppDelegate.swift
+++ EngineShed/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreData
 
+import Database // NEW
+
 @UIApplicationMain
 class AppDelegate : UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
@@ -18,6 +20,7 @@ class AppDelegate : UIResponder, UIApplicationDelegate, UISplitViewControllerDel
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        DatabaseTest().test()
         return true
     }
 
