4a7de0d9b7d1dab2f217e0de27b4db516dabeffa
Scott James Remnant <scott@netsplit.com>
2020-02-02 11:03:36 -0800

    For now, disable CloudKit sync in Release builds

--- Database/PersistentContainer.swift
+++ Database/PersistentContainer.swift
@@ -12,20 +12,30 @@ import CoreData
 ///
 /// This also turns out to be a useful place to connect the core data persistent store with the
 /// CloudKit observer and provider.
+#if DEBUG
 public final class PersistentContainer: NSPersistentCloudKitContainer {
     private override init(name: String, managedObjectModel model: NSManagedObjectModel) {
-        ValueTransformer.setValueTransformer(
-            SecureUnarchiveDateComponentsFromDataTransformer(),
-            forName: NSValueTransformerName(rawValue: "SecureUnarchiveDateComponentsFromDataTransformer"))
-
         super.init(name: name, managedObjectModel: model)
     }
+}
+#else
+public final class PersistentContainer: NSPersistentContainer {
+    private override init(name: String, managedObjectModel model: NSManagedObjectModel) {
+        super.init(name: name, managedObjectModel: model)
+    }
+}
+#endif
 
+extension PersistentContainer {
     /// Shared container instance.
     ///
     /// This is used to ensure the running app, and unit tests running within it, share the same
     /// `managedObjectModel`.
     public static let shared: PersistentContainer = {
+        ValueTransformer.setValueTransformer(
+            SecureUnarchiveDateComponentsFromDataTransformer(),
+            forName: NSValueTransformerName(rawValue: "SecureUnarchiveDateComponentsFromDataTransformer"))
+
         return PersistentContainer(name: "EngineShed")
     }()
 }
