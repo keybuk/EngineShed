03fef2d951b0847b9cbfdfed3424b15c3107e15e
Scott James Remnant <scott@netsplit.com>
2020-06-07 22:07:24 -0700

    TrainEditor - start new project in the workspace

---
+++ TrainEditor/TrainEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrainEditor
+//
+//  Created by Scott James Remnant on 6/7/20.
+//  Copyright © 2020 Scott James Remnant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
