1917cf1dcb4134eaf7a7b515607d086ab85171ad
Scott James Remnant <scott@netsplit.com>
2020-06-22 15:36:57 -0700

    RoundHouse - create new project in Xcode 12

---
+++ RoundHouse/RoundHouse/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RoundHouse
+//
+//  Created by Scott James Remnant on 6/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RoundHouse/RoundHouse/RoundHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundHouseApp.swift
+//  RoundHouse
+//
+//  Created by Scott James Remnant on 6/22/20.
+//
+
+import SwiftUI
+
+@main
+struct RoundHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
