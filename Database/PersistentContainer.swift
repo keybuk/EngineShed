feb9ffeafed7e542d56b2a6ec3006d301ef1bcd3
Scott James Remnant <scott@netsplit.com>
2020-06-22 16:01:42 -0700

    RoundHouse - add managed object context to the environment
    This is a first pass at a way this might work using the new StateModel approach

--- Database/PersistentContainer.swift
+++ Database/PersistentContainer.swift
@@ -56,3 +56,32 @@ extension PersistentContainer {
         return managedObjectContext
     }
 }
+
+extension PersistentContainer: ObservableObject {
+    public static func loadDefaultStore() -> PersistentContainer {
+        let container = PersistentContainer.shared
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+
+        // Merge changes from the store into the context automatically, these include both
+        // CloudKit sync and saves from writable contexts.
+        try? container.viewContext.setQueryGenerationFrom(.current)
+        container.viewContext.automaticallyMergesChangesFromParent = true
+
+        return container
+    }
+}
