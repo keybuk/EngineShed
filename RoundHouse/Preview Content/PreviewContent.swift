85260d30a4902011e8876b289edd3f88613712c9
Scott James Remnant <scott@netsplit.com>
2020-06-24 11:56:47 -0700

    RoundHouse - on macOS load preview assets with NSImage

--- RoundHouse/Preview Content/PreviewContent.swift
+++ RoundHouse/Preview Content/PreviewContent.swift
@@ -5,32 +5,36 @@
 //  Created by Scott James Remnant on 6/23/20.
 //
 
+#if os(iOS)
 import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
 import CoreData
 
 import Database
 
 struct PreviewContent {
     var managedObjectContext: NSManagedObjectContext
-
+    
     var purchases: [String: Purchase]
     var models: [String: Model]
     var trains: [String: Train]
-
+    
     init() {
         managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-
+        
         guard let modelURL = Bundle(identifier: "com.netsplit.EngineShed.Database")?
                 .url(forResource: "EngineShed", withExtension: "momd")
         else {
             preconditionFailure("Missing bundle or managed object model")
         }
-
+        
         guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)
         else {
             preconditionFailure("Unable to open managed object model")
         }
-
+        
         do {
             let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
             try persistentStoreCoordinator.addPersistentStore(
@@ -42,18 +46,18 @@ struct PreviewContent {
         } catch let error as NSError {
             preconditionFailure("Unable to open managed object model: \(error.localizedDescription)")
         }
-
+        
         
         purchases = [:]
         models = [:]
         trains = [:]
-
+        
         var purchase: Purchase
         var model: Model
         var train: Train
         var member: TrainMember
-
-
+        
+        
         purchase = Purchase(context: managedObjectContext)
         purchase.manufacturer = "Hornby"
         purchase.catalogNumber = "R3736"
@@ -61,11 +65,11 @@ struct PreviewContent {
         purchase.price = 272.99
         purchase.condition = .new
         purchases["R3736"] = purchase
-
+        
         train = Train(context: managedObjectContext)
         train.name = "The Flying Scotsman"
         trains["The Flying Scotsman"] = train
-
+        
         model = purchase.addModel()
         model.classification = .locomotive
         model.disposition = .normal
@@ -74,12 +78,12 @@ struct PreviewContent {
         model.name = "Flying Scotsman"
         model.livery = "LNER, Apple Green"
         model.era = .theBigFour
-        model.imageData = UIImage(named: "R3736")?.pngData()
+        model.imageData = imageData(named: "R3736")
         models["R3736"] = model
-
+        
         member = train.addMember()
         member.model = model
-
+        
         model = purchase.addModel()
         model.classification = .coach
         model.disposition = .normal
@@ -87,12 +91,12 @@ struct PreviewContent {
         model.number = "23864"
         model.livery = "LNER Teak"
         model.era = .theBigFour
-        model.imageData = UIImage(named: "R4828")?.pngData()
+        model.imageData = imageData(named: "R4828")
         models["R4828"] = model
-
+        
         member = train.addMember()
         member.model = model
-
+        
         model = purchase.addModel()
         model.classification = .coach
         model.disposition = .normal
@@ -100,12 +104,12 @@ struct PreviewContent {
         model.number = "31885"
         model.livery = "LNER Teak"
         model.era = .theBigFour
-        model.imageData = UIImage(named: "R4827")?.pngData()
+        model.imageData = imageData(named: "R4827")
         models["R4827"] = model
-
+        
         member = train.addMember()
         member.model = model
-
+        
         model = purchase.addModel()
         model.classification = .coach
         model.disposition = .normal
@@ -113,13 +117,13 @@ struct PreviewContent {
         model.number = "32557"
         model.livery = "LNER Teak"
         model.era = .theBigFour
-        model.imageData = UIImage(named: "R4826")?.pngData()
+        model.imageData = imageData(named: "R4826")
         models["R4826"] = model
-
+        
         member = train.addMember()
         member.model = model
-
-
+        
+        
         purchase = Purchase(context: managedObjectContext)
         purchase.manufacturer = "Hornby"
         purchase.catalogNumber = "R3676"
@@ -127,7 +131,7 @@ struct PreviewContent {
         purchase.price = 246.99
         purchase.condition = .new
         purchases["R3676"] = purchase
-
+        
         model = purchase.addModel()
         model.classification = .locomotive
         model.disposition = .normal
@@ -136,10 +140,10 @@ struct PreviewContent {
         model.name = "Mallard"
         model.livery = "LNER, Garter Blue"
         model.era = .theBigFour
-        model.imageData = UIImage(named: "R3676")?.pngData()
+        model.imageData = imageData(named: "R3676")
         models["R3676"] = model
-
-
+        
+        
         purchase = Purchase(context: managedObjectContext)
         purchase.manufacturer = "Hornby"
         purchase.catalogNumber = "R3691"
@@ -147,11 +151,11 @@ struct PreviewContent {
         purchase.price = 519.99
         purchase.condition = .new
         purchases["R3691"] = purchase
-
+        
         train = Train(context: managedObjectContext)
         train.number = "800010"
         trains["800010"] = train
-
+        
         model = purchase.addModel()
         model.classification = .multipleUnit
         model.disposition = .normal
@@ -159,12 +163,12 @@ struct PreviewContent {
         model.number = "815010"
         model.name = "Paddington Bear"
         model.era = .currentEra
-        model.imageData = UIImage(named: "R3691.1")?.pngData()
+        model.imageData = imageData(named: "R3691.1")
         models["R3691.1"] = model
-
+        
         member = train.addMember()
         member.model = model
-
+        
         model = purchase.addModel()
         model.classification = .multipleUnit
         model.disposition = .normal
@@ -172,12 +176,12 @@ struct PreviewContent {
         model.number = "814010"
         model.livery = "GWR Green"
         model.era = .currentEra
-        model.imageData = UIImage(named: "R3691.2")?.pngData()
+        model.imageData = imageData(named: "R3691.2")
         models["R3691.2"] = model
-
+        
         member = train.addMember()
         member.model = model
-
+        
         model = purchase.addModel()
         model.classification = .multipleUnit
         model.disposition = .normal
@@ -185,12 +189,12 @@ struct PreviewContent {
         model.number = "813010"
         model.livery = "GWR Green"
         model.era = .currentEra
-        model.imageData = UIImage(named: "R3691.3")?.pngData()
+        model.imageData = imageData(named: "R3691.3")
         models["R3691.3"] = model
-
+        
         member = train.addMember()
         member.model = model
-
+        
         model = purchase.addModel()
         model.classification = .multipleUnit
         model.disposition = .normal
@@ -198,12 +202,12 @@ struct PreviewContent {
         model.number = "812010"
         model.livery = "GWR Green"
         model.era = .currentEra
-        model.imageData = UIImage(named: "R3691.4")?.pngData()
+        model.imageData = imageData(named: "R3691.4")
         models["R3691.4"] = model
-
+        
         member = train.addMember()
         member.model = model
-
+        
         model = purchase.addModel()
         model.classification = .multipleUnit
         model.disposition = .normal
@@ -212,19 +216,29 @@ struct PreviewContent {
         model.name = "Michael Bond"
         model.livery = "GWR Green"
         model.era = .currentEra
-        model.imageData = UIImage(named: "R3691.5")?.pngData()
+        model.imageData = imageData(named: "R3691.5")
         models["R3691.5"] = model
-
+        
         member = train.addMember()
         member.model = model
-
-
+        
+        
         do {
             try managedObjectContext.save()
         } catch let error as NSError {
             preconditionFailure("Unable to save context: \(error.localizedDescription)")
         }
     }
+    
+    #if os(iOS)
+    func imageData(named name: String) -> Data? {
+        UIImage(named: name)?.pngData()
+    }
+    #elseif os(macOS)
+    func imageData(named name: String) -> Data? {
+        NSImage(named: name)?.pngData()
+    }
+    #endif
 }
 
 let previewContent = PreviewContent()
