0e4f43d62a43f4345b4f2aa1e2bd9da2f6c179b5
Scott James Remnant <scott@netsplit.com>
2021-06-09 19:16:30 -0700

    Add RoundHouse Project
    Re-use the same name for the SwiftUI project as before

---
+++ RoundHouse/Shared/RoundHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundHouseApp.swift
+//  Shared
+//
+//  Created by Scott James Remnant on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct RoundHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
